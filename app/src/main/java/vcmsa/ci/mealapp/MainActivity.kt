98b9041e44d625353c64490a6d7b638304d30f49
lab_services_student <st10320506@vcconnect.edu.za>
2025-04-01 10:17:28 +0200

    updated referencing comments

--- app/src/main/java/vcmsa/ci/mealapp/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/mealapp/MainActivity.kt
@@ -28,54 +28,49 @@ class MainActivity : AppCompatActivity() {
         //Declaring the button function that when clicked will generate the meal suggestions
         generateMealButton.setOnClickListener {
             //If statements for when the user enters different times of the day
-            //Reference: https://github.com/liehanels/NumberGuesser
+            //Reference: https://github.com/liehanels/NumberGuesser - Liehan Els, 2025. Liehan had written code to help with understanding if statements. [online] Available at: <https://github.com/liehanels/NumberGuesser> [Accessed 31 March 2025].
             if(inputTime.text.toString() == "Morning") {
                 //Intent code is used to pass this information "Morning" to the second screen (Generated Suggestions Activity)
-                //Reference: https://github.com/liehanels/UserInterface
+                //Reference: https://github.com/liehanels/UserInterface - Liehan Els, 2025. Liehan had written code to show how to pass information between two screens/activities. [online] Available at: <https://github.com/liehanels/UserInterface> [Accessed 31 March 2025].
                 val intent = Intent (this, GeneratedSuggestionsActivity::class.java)
                 intent.putExtra("inputTime", inputTime.text.toString())
                 startActivity(intent)
 
             } else if (inputTime.text.toString() == "Mid-morning") {
                 //Intent code is used to pass this information "Mid-morning" to the second screen (Generated Suggestions Activity)
-                //Reference: https://github.com/liehanels/UserInterface
                 val intent = Intent (this, GeneratedSuggestionsActivity::class.java)
                 intent.putExtra("inputTime", inputTime.text.toString())
                 startActivity(intent)
 
             } else if (inputTime.text.toString() == "Afternoon") {
                 //Intent code is used to pass this information "Afternoon" to the second screen (Generated Suggestions Activity)
-                //Reference: https://github.com/liehanels/UserInterface
                 val intent = Intent(this, GeneratedSuggestionsActivity::class.java)
                 intent.putExtra("inputTime", inputTime.text.toString())
                 startActivity(intent)
 
             } else if (inputTime.text.toString() == "Mid-afternoon") {
                 //Intent code is used to pass this information "Mid-afternoon" to the second screen (Generated Suggestions Activity)
-                //Reference: https://github.com/liehanels/UserInterface
                 val intent = Intent(this, GeneratedSuggestionsActivity::class.java)
                 intent.putExtra("inputTime", inputTime.text.toString())
                 startActivity(intent)
 
             } else if (inputTime.text.toString() == "Dinner") {
                 //Intent code is used to pass this information "Dinner" to the second screen (Generated Suggestions Activity)
-                //Reference: https://github.com/liehanels/UserInterface
                 val intent = Intent(this, GeneratedSuggestionsActivity::class.java)
                 intent.putExtra("inputTime", inputTime.text.toString())
                 startActivity(intent)
 
             } else if(inputTime.text.toString() == "Dessert") {
                 //Intent code is used to pass this information "Dessert" to the second screen (Generated Suggestions Activity)
-                //Reference: https://github.com/liehanels/UserInterface
                 val intent = Intent(this, GeneratedSuggestionsActivity::class.java)
                 intent.putExtra("inputTime", inputTime.text.toString())
                 startActivity(intent)
             }
             //the final else statement displays an error message when the user has entered incorrect information
-            // Reference: https://github.com/liehanels/NumberGuesser
+            //Reference: https://github.com/liehanels/NumberGuesser - Liehan Els, 2025. Liehan had written code to help with understanding if statements. [online] Available at: <https://github.com/liehanels/NumberGuesser> [Accessed 31 March 2025].
             else {
                 //Toast message to display a prompt when the user has entered incorrect information
-                //Reference: https://github.com/liehanels/NumberGuesser
+                //Reference: https://github.com/liehanels/NumberGuesser - Liehan Els, 2025. Liehan had written code to help with understanding how to use a TOAST message. [online] Available at: <https://github.com/liehanels/NumberGuesser> [Accessed 31 March 2025].
                 Toast.makeText(this, "Please enter a valid time of day", Toast.LENGTH_SHORT).show()
             }
         }
